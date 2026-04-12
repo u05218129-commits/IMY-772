e19b867a285846ab83f1428f5e7da5fe5c5daa04
sine0326 <u26855047@tuks.co.za>
2026-04-12 18:16:43 +0200

    backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 // This is to register the Amplify data backend
 defineBackend({
+  auth,
   data,
 });
