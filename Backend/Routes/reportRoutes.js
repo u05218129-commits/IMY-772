dda20442f66777ea7aba5e673c557dd8c761190a
Sine <u26855047@tuks.co.za>
2026-04-26 22:46:16 +0200

    Update reportRoutes.js

--- Backend/Routes/reportRoutes.js
+++ Backend/Routes/reportRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { submittingReport, getAllReports } = require('../Controller/Reports');
-const { verifyToken, verifyAdmin } = require('../middleware/AuthenticationMiddleware');
+const {submittingReport, getAllReports} = require('../Controller/Reports');
+const {verifyToken, verifyAdmin} = require('../middleware/AuthenticationMiddleware');
 
-//FR11.1: Submitting a report
+//FR11.1 - Submitting a report
 router.post('/submit', submittingReport);
 
-//FR11.1: Getting the reports.
+//FR11.1 - Getting the reports.
 router.get('/all', verifyToken, verifyAdmin, getAllReports);
 module.exports = router;
